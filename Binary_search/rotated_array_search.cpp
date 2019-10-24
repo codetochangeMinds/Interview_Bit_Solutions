2642441162727b3395c8b0022fc26c64cad040cf
CodetochangeMinds <2016ucs0017@iitjammu.ac.in>
2019-10-24 23:30:25 +0530

    sol of the problem using two pointer

--- Binary_search/rotated_array_search.cpp
+++ Binary_search/rotated_array_search.cpp
@@ -1,3 +1,12 @@
+// Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2 ).
+
+// You are given a target value to search. If found in the array, return its index, otherwise return -1.
+
+// You may assume no duplicate exists in the array.
+
+
 // the function find the pivot element
 // pivot is the element which is greater than both 
 // its neighboring element, by finding the pivot element 
